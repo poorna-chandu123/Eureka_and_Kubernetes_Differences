249b9a526dcd6c782267e013a5567f0dfefbdf82
poorna-chandu123 <mechanduloves@gmail.com>
2026-09-25 23:06:31 +0530

    added DB PSW and JWT_SECRET key in YML files

--- common/auth-server/src/main/java/com/eurekakubernetes/authserver/AuthServerApplication.java
+++ common/auth-server/src/main/java/com/eurekakubernetes/authserver/AuthServerApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
+
+        System.out.println("AuthServerApplication is running ...!!!");
     }
 }
--- common/config-server/src/main/java/com/eurekakubernetes/configserver/ConfigServerApplication.java
+++ common/config-server/src/main/java/com/eurekakubernetes/configserver/ConfigServerApplication.java
@@ -10,5 +10,7 @@ public class ConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+
+        System.out.println("ConfigServerApplication is running ...!!!");
     }
 }
